4dfaf3f9069de1f66cc7e9134949224147d7195c
muktadira22 <aryamuktadir22@gmail.com>
2021-08-30 21:38:35 +0700

    adding tailwinds css

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import ThemeContext from './stores/theme/context';
+import React from 'react';
 
 function App() {
+  const context = React.useContext(ThemeContext)
+
+  console.log("context", context)
+
+  const darkMode = () => {
+    context.toggleTheme('dark')
+  }
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload. <button onClick={darkMode}>test button</button>
         </p>
         <a
           className="App-link"
@@ -16,6 +25,8 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
+
+          {JSON.stringify(context.state)}
         </a>
       </header>
     </div>
